4124748e849709a1ceac579cd5ebd095b35cef5c
Ademilson Bertolin <ademilsonbertolinnn@gmail.com>
2021-09-26 11:18:02 -0300

    ArquiteturaController

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_TYPEPONTO.Controllers
+{
+    public class FuncionarioController
+    {
+        
+    }
+}
