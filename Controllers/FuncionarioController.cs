048ed84a2a2672708347102ba73f91a3b3279026
Ademilson Bertolin <ademilsonbertolinnn@gmail.com>
2021-09-30 16:07:08 -0300

    Model Funcionário

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace Backend_TYPEPONTO.Controllers
 {
-    public class FuncionarioController
+    [@ApiController]
+    [Route("api/funcionario")]
+    public class FuncionarioController : ControllerBase
     {
-        
+        //POST: api/funcionario/
+        [HttpPost]
+        [Route("create")]
+        public Funcionario Create(Funcionario funcionario)
+        {
+            return funcionario;
+        }
     }
 }
---
+++ models/Funcionario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_TYPEPONTO.models
+{
+    public class Funcionario
+    {
+        public string nome { get; set; }
+        public int pis { get; set; }
+        public string dataAdm { get; set; }
+        
+    }
+}
